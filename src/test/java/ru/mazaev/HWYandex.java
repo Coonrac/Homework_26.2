2d05c0c8cd09a315580e80bf5dd852f839375555
Raccoon <andrej.mazaev@mail.ru>
2021-04-01 22:39:26 +0300

    Initial commit

---
+++ src/test/java/ru/mazaev/HWYandex.java
@@ -0,0 +1,37 @@
+package ru.mazaev;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class HWYandex {
+
+    WebDriver webDriver;
+
+    @Test
+    public void HelloWorld(){
+        WebDriverManager.chromedriver().setup();
+
+        webDriver = new ChromeDriver();
+        webDriver.get("https://www.yandex.ru");
+
+        WebElement searchInput = webDriver.findElement(By.xpath("//*[@id=\"text\"]")); // Ищем оле ввода
+        searchInput.sendKeys("Hello World"); // Вбиваем тебуемый текст в поле ввода
+        searchInput.submit(); // Производим поиск в поисковике
+
+        String title = webDriver.getTitle();
+        Assert.assertTrue(title.contains("Hello World")); // Проверяем в имени вкладки текст
+    }
+
+    @After
+    public void closeDriver(){
+        if (webDriver != null) {
+            webDriver.quit();
+        }
+    }
+}
